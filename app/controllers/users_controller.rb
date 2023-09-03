a8442eca48d8058def42d55e6b1a67dd940013de
54chair <takukoshi3721@gmail.com>
2023-09-04 01:11:59 +0900

    reviewしたお店一覧とreview一覧を表示する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @reviews = @user.reviews
+    @shops = Shop.where(id: @reviews.pluck(:shop_id))
   end
 end
