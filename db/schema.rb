0bf29f5636cd25663337d509143dd44a7171d1b5
54chair <takukoshi3721@gmail.com>
2023-09-03 23:23:04 +0900

    Userモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_03_142102) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_03_142230) do
   create_table "shops", force: :cascade do |t|
     t.string "name"
     t.float "latitude"
     t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
